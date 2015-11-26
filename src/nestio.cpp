e2fe9b9b3de618ad252710ba7b7675c9681c5501
Sebastian Billaudelle <sbillaudelle@gmail.com>
2015-11-26 17:12:44 +0100

    Fixed data types

--- src/nestio.cpp
+++ src/nestio.cpp
@@ -49,7 +49,7 @@ Reader::Reader(std::string filename)
     sion_seek(sid, task, last_chunk, 0);
     sion_int64 end = sion_bytes_avail_in_block(sid);
 
-    size_t tail_size = 2 * sizeof(int) + 2 * sizeof(sion_int64) + 3 * sizeof(double);
+    size_t tail_size = 4 * sizeof(sion_int64) + 3 * sizeof(double);
 
     // check if tail section was splitted over two chunks and fix seeking
     if (tail_size > end)
@@ -62,12 +62,11 @@ Reader::Reader(std::string filename)
     sion_seek(sid, task, last_chunk, end - tail_size);
 
     // read tail
-    int body_blk, info_blk;
-    sion_int64 body_pos, info_pos;
+    sion_int64 body_blk, info_blk, body_pos, info_pos;
 
-    sion_fread(&body_blk, sizeof(int), 1, sid);
+    sion_fread(&body_blk, sizeof(sion_int64), 1, sid);
     sion_fread(&body_pos, sizeof(sion_int64), 1, sid);
-    sion_fread(&info_blk, sizeof(int), 1, sid);
+    sion_fread(&info_blk, sizeof(sion_int64), 1, sid);
     sion_fread(&info_pos, sizeof(sion_int64), 1, sid);
 
     double t_start, t_end, duration;
@@ -79,24 +78,25 @@ Reader::Reader(std::string filename)
     sion_seek(sid, task, info_blk, info_pos);
 
     int n_dev;
-    sion_fread(&n_dev, sizeof(int), 1, sid);
+    sion_fread(&n_dev, sizeof(sion_uint64), 1, sid);
 
     for (size_t i = 0; i < n_dev; ++i)
     {
-        int dev_id, type;
+        sion_uint64 dev_id;
+		sion_uint32 type;
         char name[16];
         char label[16];
-        sion_fread(&dev_id, sizeof(int), 1, sid);
-        sion_fread(&type, sizeof(int), 1, sid);
+        sion_fread(&dev_id, sizeof(sion_uint64), 1, sid);
+        sion_fread(&type, sizeof(sion_uint32), 1, sid);
         sion_fread(&name, sizeof(char), 16, sid);
         sion_fread(&label, sizeof(char), 16, sid);
 
         int n_rec;
-        sion_fread(&n_rec, sizeof(int), 1, sid);
+        sion_fread(&n_rec, sizeof(sion_uint64), 1, sid);
 
         int n_val;
-        sion_fread(&n_val, sizeof(int), 1, sid);
-        
+        sion_fread(&n_val, sizeof(sion_uint32), 1, sid);
+
 		std::vector<std::string> observables;
         for (size_t j = 0; j < n_val; ++j)
         {
@@ -106,8 +106,7 @@ Reader::Reader(std::string filename)
         }
 
         size_t entry_size = sizeof(double) + sizeof(double) + n_val * sizeof(double);
-
-        std::pair<size_t, size_t> shape = std::make_pair(n_rec, n_val + 2);
+        std::pair<size_t, size_t> shape = std::make_pair(n_rec, n_val + 3);
 
         DeviceData& entry = data_.insert(std::make_pair(dev_id, DeviceData(shape))).first->second;
         entry.gid = dev_id;
@@ -118,8 +117,7 @@ Reader::Reader(std::string filename)
 
     for (int task = 0; task < ntasks; ++task)
     {
-        int body_blk, info_blk;
-        sion_int64 body_pos, info_pos;
+        sion_int64 body_blk, info_blk, body_pos, info_pos;
 
         if (task == 0)
         {
@@ -129,13 +127,13 @@ Reader::Reader(std::string filename)
 
             sion_int64 end = sion_bytes_avail_in_block(sid);
 
-            size_t tail_size = 2 * sizeof(int) + 2 * sizeof(sion_int64) + 3 * sizeof(double);
+            size_t tail_size = 4 * sizeof(sion_int64) + 3 * sizeof(double);
             sion_seek(sid, task, last_chunk, end - tail_size);
 
             // read tail
-            sion_fread(&body_blk, sizeof(int), 1, sid);
+            sion_fread(&body_blk, sizeof(sion_int64), 1, sid);
             sion_fread(&body_pos, sizeof(sion_int64), 1, sid);
-            sion_fread(&info_blk, sizeof(int), 1, sid);
+            sion_fread(&info_blk, sizeof(sion_int64), 1, sid);
             sion_fread(&info_pos, sizeof(sion_int64), 1, sid);
         }
         else
@@ -151,10 +149,11 @@ Reader::Reader(std::string filename)
 
         sion_int64 position_ = reader.get_position();
 
-        int device_gid;
-        int neuron_gid;
-        double time;
-        int n_values;
+        sion_uint64 device_gid;
+        sion_uint64 neuron_gid;
+		sion_int64 step;
+        double offset;
+        sion_uint32 n_values;
 
         sion_int64 current_blk = reader.get_chunk();
         sion_int64 current_pos = reader.get_position();
@@ -163,11 +162,12 @@ Reader::Reader(std::string filename)
         {
             reader.read(&device_gid, 1);
             reader.read(&neuron_gid, 1);
-            reader.read(&time, 1);
+            reader.read(&step, 1);
+            reader.read(&offset, 1);
             reader.read(&n_values, 1);
 
             RawMemory& buffer = data_.find(device_gid)->second.data;
-            buffer << (double) neuron_gid << time;
+            buffer << (double) neuron_gid << (double) step << offset;
 
             for (int i = 0; i < n_values; ++i)
             {
