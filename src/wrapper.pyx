d2b257faecea93be7b123854e4334a8355efe8d5
Alexander Peyser <a.peyser@fz-juelich.de>
2016-07-07 13:18:52 +0200

    respace wrapper & add all properties

--- src/wrapper.pyx
+++ src/wrapper.pyx
@@ -1,6 +1,6 @@
 from libcpp.vector cimport vector
 from libcpp.string cimport string
-from libc.stdint cimport uint64_t
+from libc.stdint cimport uint64_t, uint32_t
 
 import numpy as np
 cimport numpy as np
@@ -8,21 +8,29 @@ from cython.view cimport array as cvarray
 
 
 cdef extern from "nestio.h":
-        cdef cppclass CDeviceData "DeviceData":
-                CDeviceData(size_t, size_t) except +
-                char* get_data() except +
+    cdef cppclass CDeviceData "DeviceData":
+        CDeviceData(size_t, size_t) except +
 
-                uint64_t gid;
-                string name;
-                string label;
+        char* get_data() except +
 
-                size_t rows;
-                size_t values;
+        uint64_t gid;
+        uint32_t type;
+        string name;
+        string label;
+        vector[string] observables;
 
-        cdef cppclass CNestReader "NestReader":
-                CNestReader(string) except +
-                CDeviceData* get_device_data(int) except +
-                vector[int] list_devices()
+        size_t rows;
+        size_t values;
+
+    cdef cppclass CNestReader "NestReader":
+        CNestReader(string) except +
+
+        CDeviceData* get_device_data(int) except +
+        vector[int] list_devices()
+
+        double get_start() except +
+        double get_end() except +
+        double get_duration() except +
 
 class DevArray(np.ndarray):
     # need to keep a reference to the DeviceData object
@@ -36,57 +44,77 @@ class DevArray(np.ndarray):
         self.device = getattr(obj, 'device', None)
 
 cdef class DeviceData:
-        cdef CDeviceData* entry
-        cdef object nestReader # keep a reference for the parent
+    cdef CDeviceData* entry
+    cdef object nestReader # keep a reference for the parent
 
-        def __cinit__(self, long entry_ptr, nestReader):
-            self.entry = <CDeviceData*> entry_ptr
-            self.nestReader = nestReader
+    def __cinit__(self, long entry_ptr, NestReader nestReader):
+        self.entry = <CDeviceData*> entry_ptr
+        self.nestReader = nestReader
 
-        def __dealloc__(self):
-            self.nestReader = None
+    def __dealloc__(self):
+        self.nestReader = None
 
-        property data:
-                def __get__(self):
-                    # underlying object: [gid=uint64, int64=step, offset=double, values....][shape[0]]
-                    # Length of row: shape[1]
-                    cdef size_t rows = self.entry.rows
-                    cdef size_t values = self.entry.values
-                    cdef char* data = self.entry.get_data()
-                    cdef str dtype = "=Q=q=d={}d".format(values)
-                    cdef tuple dim = (rows,)
-                    cdef cvarray buf = cvarray(data, shape=dim, dtype=dtype)
-                    
-                    return DevArray(buf, self, dtype)
-        
-        property name:
-                def __get__(self):
-                        return self.entry.name
-        
-        property label:
-                def __get__(self):
-                        return self.entry.name
-        
-        property gid:
-                def __get__(self):
-                        return self.entry.gid
+    property data:
+        def __get__(self):
+            # underlying object: [gid=uint64, int64=step, offset=double, values....][shape[0]]
+            # Length of row: shape[1]
+            cdef size_t rows = self.entry.rows
+            cdef size_t values = self.entry.values
+            cdef char* data = self.entry.get_data()
+            cdef str dtype = "=Q=q=d={}d".format(values)
+            cdef tuple dim = (rows,)
+            cdef cvarray buf = cvarray(data, shape=dim, dtype=dtype)
+
+            return DevArray(buf, self, dtype)
+
+    property gid:
+        def __get__(self):
+            return self.entry.gid
+
+    property dtype:
+        def __get__(self):
+            return self.entry.type
+
+    property name:
+        def __get__(self):
+            return self.entry.name
+
+    property label:
+        def __get__(self):
+            return self.entry.name
+
+    property observables:
+        def __get__(self):
+            return <list> self.entry.observables
 
 cdef class NestReader:
-        cdef CNestReader* reader
-        def __init__(self, filename):
-                self.reader = new CNestReader(filename)
+    cdef CNestReader* reader
+    def __init__(self, filename):
+        self.reader = new CNestReader(filename)
 
-        def __dealloc__(self):
-            del self.reader
+    def __dealloc__(self):
+        del self.reader
         
-        def __iter__(self):
-                cdef vector[int] gids = self.reader.list_devices()
-                cdef CDeviceData* device_data_ptr;
-                for g in gids:
-                        device_data_ptr = self.reader.get_device_data(g)
-                        yield DeviceData(<long> device_data_ptr)
+    def __iter__(self):
+        cdef vector[int] gids = self.reader.list_devices()
+        cdef CDeviceData* device_data_ptr;
+        for g in gids:
+            device_data_ptr = self.reader.get_device_data(g)
+            yield DeviceData(<long> device_data_ptr)
 
-        def __getitem__(self, device_gid):
-                cdef CDeviceData* device_data_ptr = self.reader.get_device_data(device_gid)
-                dev_data = DeviceData(<long> device_data_ptr)
-                return dev_data
+    def __getitem__(self, device_gid):
+        cdef CDeviceData* device_data_ptr = self.reader.get_device_data(device_gid)
+        dev_data = DeviceData(<long> device_data_ptr)
+        return dev_data
+
+    property t_start:
+        def __get__(self):
+            return self.reader.get_start()
+
+    property t_end:
+        def __get__(self):
+            return self.reader.get_end()
+
+    property duration:
+        def __get__(self):
+            return self.reader.get_duration()
